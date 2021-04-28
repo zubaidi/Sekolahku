087acb3aba1cc9864263093ae5f65fe24d9ba473
lab rpl <xi.rplsmksa@gmail.com>
2021-04-28 11:52:18 +0700

    Penambahan event

---
+++ app/src/main/java/com/mzzub/sekolahku/JadwalActivity.kt
@@ -0,0 +1,12 @@
+package com.mzzub.sekolahku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JadwalActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jadwal)
+    }
+}
--- app/src/main/java/com/mzzub/sekolahku/MainActivity.kt
+++ app/src/main/java/com/mzzub/sekolahku/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.mzzub.sekolahku
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var jadwal:CardView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun menuJadwal(view: View) {}
+    fun menuJadwal(view: View) {
+        jadwal = findViewById(R.id.idMenuJadwal)
+        val intent = Intent(this, JadwalActivity::class.java)
+        startActivity(intent)
+    }
     fun menuDataGuru(view: View) {}
     fun menuGaleri(view: View) {}
     fun menuBKK(view: View) {}
