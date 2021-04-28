f4453fc4397dc4d5b5c88dc1242db1bb137a9072
lab rpl <xi.rplsmksa@gmail.com>
2021-04-28 09:56:04 +0700

    Penambahan event

--- app/src/main/java/com/mzzub/sekolahku/MainActivity.kt
+++ app/src/main/java/com/mzzub/sekolahku/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mzzub.sekolahku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +10,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun menuJadwal(view: View) {}
+    fun menuDataGuru(view: View) {}
+    fun menuGaleri(view: View) {}
+    fun menuBKK(view: View) {}
+    fun menuJurusan(view: View) {}
+    fun menuBerita(view: View) {}
+    fun menuPPDB(view: View) {}
+    fun menuTentang(view: View) {}
 }
